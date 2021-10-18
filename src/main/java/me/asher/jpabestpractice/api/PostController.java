140dc205d28db520e0bd56b7c3150f93e90a52d7
choiasher <tahun970@gmail.com>
2021-10-18 22:59:31 +0900

    add hateoas api example

--- src/main/java/me/asher/jpabestpractice/api/PostController.java
+++ src/main/java/me/asher/jpabestpractice/api/PostController.java
@@ -5,25 +5,35 @@ import me.asher.jpabestpractice.domain.Post;
 import me.asher.jpabestpractice.domain.PostRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @RestController
 public class PostController {
 
     private final PostRepository posts;
+    private final PagedResourcesAssembler<Post> assembler;
 
     @GetMapping("/posts/{id}")
     public String getAPost(@PathVariable("id") Post post) {
         return post.getTitle();
     }
 
-    @GetMapping("/posts")
-    public Page<Post> getPosts(Pageable pageable) {
+    @GetMapping("/api/v1/posts")
+    public Page<Post> getPostsV1(Pageable pageable) {
         return posts.findAll(pageable);
+    }
+
+    @GetMapping("/api/v2/posts")
+    public ResponseEntity<PagedModel<EntityModel<Post>>> getPostsV2(Pageable pageable) {
+        Page<Post> customers = posts.findAll(pageable);
+        PagedModel<EntityModel<Post>> entityModels = assembler.toModel(customers);
+        return ResponseEntity.ok(entityModels);
     }
 }
